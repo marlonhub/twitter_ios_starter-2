7ca5ee8cc90948b78f5a0cc3492066a97e7f0ce1
marlonhub <60769738+marlonhub@users.noreply.github.com>
2022-02-24 13:39:29 -0500

    Submission

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Marlon Cuna on 2/17/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+    self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }
+    }
+   
+        @IBAction func onLoginButton(_ sender: Any) {
+            
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+            
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
+        
+    }
+    
+ 
+
+}
---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,104 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Marlon Cuna on 2/17/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    
+ 
+    @IBAction func tweetFavorite(_ sender: Any) {
+        
+        let tobeFavorited = !favorited
+        if (tobeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(isFavorited: true)
+                
+            }, failure: { (error) in
+                print("Favorite did not work: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: { self.setFavorite(isFavorited: false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+                
+            })
+        }
+    }
+
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(isRetweeted: true) 
+        },  failure: {  (error) in
+            print("Error with retweeting \(Error.self)")
+        })
+    }
+
+    func setRetweeted( isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+   
+    
+    
+    func setFavorite( isFavorited:Bool) {
+        favorited = isFavorited
+        if(favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
